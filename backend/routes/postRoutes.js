55763ea22cbdeb01dfb0cc5b438732b578b7c7d7
botketan <ketansingh918@gmail.com>
2023-11-27 18:37:14 +0530

    Minor fix

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,17 +1,15 @@
 import express from "express";
 import {create} from "../controllers/post.js"
+import { getByCommunity,getbyparent,getById,deletePost } from "../controllers/post.js";
+
+
 const router = express.Router();
-import { getByCommunity } from "../controllers/post.js";
-import { getbyparent } from "../controllers/post.js";
-import { getById } from "../controllers/post.js"
 
 router.post("/getbyparent", getbyparent);
 router.get("/postid",getById);
 router.get("/community",getByCommunity);
-
-import {deletePost} from "../controllers/posts.js";
-
 router.delete("/", deletePost);
-
 router.post('/create',create);
+
+
 export default router;
