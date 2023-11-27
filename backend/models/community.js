850b1767ebcf4125b0833c62ffc0d311f651732d
Dev Shah <dev.shah@iitg.ac.in>
2023-11-27 15:38:51 +0530

    ref user

--- backend/models/community.js
+++ backend/models/community.js
@@ -7,7 +7,8 @@ const communitySchema = mongoose.Schema({
     required: true,
     },
   userId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: `user`,
     required: true,
     },
   isPublished: {
