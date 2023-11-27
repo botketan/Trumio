fea943e13540930733b8f9fe92d6c8163ba732fb
Dev Shah <dev.shah@iitg.ac.in>
2023-11-27 17:54:15 +0530

    post delete route

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,2 +1,30 @@
 import { user } from "../models/user.js";
 import { post } from "../models/post.js";
+
+export const deletePost = async (req, res) => {
+  const post_id = req.params.id;
+  try {
+    await deletePostRecursive(post_id);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+  return res.status(200).json({ message: "Post deleted successfully" });
+}
+
+const deletePostRecursive = async (post_id) => {
+    const postget = await post.findById(post_id);
+    if (!postget) {
+      throw new Error("Post not found");
+    }
+    post.find({parentPost:post_id},function(err,docs){
+      if(err){
+          console.log(err);
+      }
+      else{
+          docs.forEach(function(doc){
+              deletePostRecursive(doc._id);
+              console.log(doc._id);
+          })
+      }})
+    await post.findByIdAndDelete(post_id);
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
 const router = express.Router();
 
+import {deletePost} from "../controllers/posts.js";
+
+router.delete("/", deletePost);
 
 export default router;
