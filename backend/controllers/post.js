deec25d5f810be82256a68a7eb96781739d1ec16
Suppandi <121857153+Suppandi-1@users.noreply.github.com>
2023-11-27 17:48:14 +0530

    created Function and route for getByCommunity

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,2 +1,8 @@
 import { user } from "../models/user.js";
 import { post } from "../models/post.js";
+
+export const getByCommunity = async (req,res) =>{
+    const {communityID } = req.body;
+    const data = await post.find({community:communityID,parentPost:NULL});
+    res.status(200).send(data);
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 const router = express.Router();
+import { getByCommunity } from "../controllers/post.js";
 
+router.get("/community",getByCommunity);
 
 export default router;
