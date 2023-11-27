af43a1774012ec3abc3f76ba3eb0c29a7d80d988
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2023-11-27 18:32:05 +0530

    fix

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,18 +1,15 @@
 import { user } from "../models/user.js";
 import { post } from "../models/post.js";
 
-<<<<<<< HEAD
 export const getById = async (req,res) =>{
     const {postId,userId}=req.body;
     const data = await post.findById({postId});
     if (data.isPublished || data.userId===userId){
         res.status(200).send(data);
     }
-    
-=======
+}
 export const getByCommunity = async (req,res) =>{
     const {communityID } = req.body;
     const data = await post.find({community:communityID,parentPost:NULL});
     res.status(200).send(data);
->>>>>>> deec25d5f810be82256a68a7eb96781739d1ec16
 }
