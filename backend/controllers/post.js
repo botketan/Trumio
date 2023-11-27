67d8bf84bb468e06bece0de27c351694ab90600c
ELIKA <elika.ac@gmail.com>
2023-11-27 18:21:24 +0530

    get by id

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,2 +1,11 @@
 import { user } from "../models/user.js";
 import { post } from "../models/post.js";
+
+export const getById = async (req,res) =>{
+    const {postId,userId}=req.body;
+    const data = await post.findById({postId});
+    if (data.isPublished || data.userId===userId){
+        res.status(200).send(data);
+    }
+    
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
 const router = express.Router();
 
+import { getById } from "../controllers/post.js"
+
+router.get("/postid",getById);
 
 export default router;
