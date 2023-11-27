829a2add58015c227cd04dd0ee261d85122036ee
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2023-11-27 19:23:15 +0530

    fix published

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getByCommunity,getbyparent,getById,deletePost,updatePost,create } from "../controllers/post.js";
+import { getByCommunity,getbyparent,getById,deletePost,updatePost,create,published } from "../controllers/post.js";
 
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.get("/postid",getById);
 router.get("/community",getByCommunity);
 router.delete("/", deletePost);
 router.post('/create',create);
+router.get('/publish', published);
 
 
 export default router;
