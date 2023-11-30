2ff314ed43c6e231ee18c8fd209798ac2865f4ef
Achintya <achintya7567@gmail.com>
2023-11-30 15:49:14 +0530

    Config tailwind, moon.io and basic Post structure

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,49 @@
+import {
+  BlockNoteView,
+  getDefaultReactSlashMenuItems,
+  ReactSlashMenuItem,
+  useBlockNote,
+} from "@blocknote/react";
+import "@blocknote/core/style.css";
+import { HiOutlineGlobeAlt } from "react-icons/hi";
+
+// Command to insert an Embed of a post in a new block below.
+const insertEmbed = (editor) => {
+  // Block that the text cursor is currently in.
+  const currentBlock = editor.getTextCursorPosition().block;
+
+  // New block we want to insert.
+  const embedBlock = {
+    type: "paragraph",
+    content: [{ type: "text", text: "Embedded post here", styles: { bold: true } }],
+  };
+
+  // Inserting the new block before the current one.
+  editor.insertBlocks([embedBlock], currentBlock, "before");
+};
+
+// Custom Slash Menu item which executes the above function.
+const insertEmbedPostItem = {
+  name: "Embed Post",
+  execute: insertEmbed,
+  aliases: ["embed", "ed"],
+  group: "Other",
+  icon: <HiOutlineGlobeAlt size={18} />,
+  hint: "Used to embed a post below.",
+};
+
+// List containing all default Slash Menu Items, as well as our custom one.
+const customSlashMenuItemList = [
+  ...getDefaultReactSlashMenuItems(),
+  insertEmbedPostItem,
+];
+
+export default function Post() {
+  // Creates a new editor instance.
+  const editor = useBlockNote({
+    slashMenuItems: customSlashMenuItemList,
+  });
+
+  // Renders the editor instance.
+  return <BlockNoteView editor={editor} />;
+}
