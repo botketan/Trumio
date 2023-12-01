93ddb2deef7cd810255f94bcf0cd6a200795f20a
Nikhil Raj <raj25222004@gmail.com>
2023-12-01 20:12:18 +0530

    updated navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GenericBrowser,
   GenericEdit,
@@ -10,29 +10,35 @@ import {
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [selected,setSelected]=useState(null)
+
+  const handleClick = (data) => {
+    setSelected(data)
+  };
+
   return (
     <nav className="flex items-center justify-between bg-gray-800 text-white p-4">
-      <Link to="/development" className="flex flex-col items-center ml-4">
+      <Link to="/development" onClick={()=>handleClick("development")} className={selected==='development'?"flex flex-col items-center ml-4 border-b border-solid border-white transition-transform duration-100 ease-in-out transform hover:scale-105 " : "flex flex-col items-center ml-4 hover:scale-105 duration-100"} >
         <GenericBrowser className="text-moon-24"/>
         <span>Development</span>
       </Link>
-      <Link to="/design" className="flex flex-col items-center">
+      <Link to="/design" onClick={()=>handleClick("design")} className={selected==='design'?"flex flex-col items-center ml-4 border-b border-solid border-white transition-transform duration-100 ease-in-out transform hover:scale-105" : "flex flex-col items-center ml-4 hover:scale-105 duration-100"} >
         <GenericEdit className="text-moon-24"/>
         <span>Design</span>
       </Link>
-      <Link to="/marketing" className="flex flex-col items-center">
+      <Link to="/marketing" onClick={()=>handleClick("marketing")} className={selected==='marketing'?"flex flex-col items-center ml-4 border-b border-solid border-white transition-transform duration-100 ease-in-out transform hover:scale-105" : "flex flex-col items-center ml-4 hover:scale-105 duration-100"} >
         <DevicesMacbookAndIphone className="text-moon-24"/>
         <span>Marketing</span>
       </Link>
-      <Link to="/business" className="flex flex-col items-center">
+      <Link to="/business" onClick={()=>handleClick("business")} className={selected==='business'?"flex flex-col items-center ml-4 border-b border-solid border-white transition-transform duration-100 ease-in-out transform hover:scale-105" : "flex flex-col items-center ml-4 hover:scale-105 duration-100"} >
         <OtherAgent className="text-moon-24"/>
         <span>Business</span>
       </Link>
-      <Link to="/data-science" className="flex flex-col items-center">
+      <Link to="/data-science" onClick={()=>handleClick("data-science")} className={selected==='data-science'?"flex flex-col items-center ml-4 border-b border-solid border-white transition-transform duration-100 ease-in-out transform hover:scale-105" : "flex flex-col items-center ml-4 hover:scale-105 duration-100"} >
         <MediaMonitor className="text-moon-24"/>
         <span>Data Science</span>
       </Link>
-      <Link to="/other" className="flex flex-col items-center mr-4">
+      <Link to="/other" onClick={()=>handleClick("other")} className={selected==='other'?"flex flex-col items-center mr-4 border-b border-solid border-white transition-transform duration-100 ease-in-out transform hover:scale-105" : "flex flex-col items-center ml-4 hover:scale-105 duration-100"} >
         <GenericBurgerRegular className="text-moon-24"/>
         <span>Other</span>
       </Link>
