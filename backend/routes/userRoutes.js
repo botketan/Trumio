8084ad8a820b5013b6b89df27f51b4bf0b1c00ff
botketan <ketansingh918@gmail.com>
2023-12-02 16:58:56 +0530

    getUser implemented

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { login,signup } from "../controllers/user.js";
+import { login,signup,getUser } from "../controllers/user.js";
 
 router.post("/login", login);
 router.post("/signup", signup);
+router.post("/getUser", getUser);
 
 export default router;
