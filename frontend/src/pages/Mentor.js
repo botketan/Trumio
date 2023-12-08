69b543e03453bed8b68c679719d69792a5892f86
Dev Shah <dev.shah@iitg.ac.in>
2023-12-08 22:18:46 +0530

    Change mentorcard size

--- frontend/src/pages/Mentor.js
+++ frontend/src/pages/Mentor.js
@@ -9,17 +9,6 @@ function Mentor() {
     const [selected,setSelected]=useState("")
     const [mentors,setMentors]=useState([]);
     const [filteredMentors,setFilteredMentors]=useState([]);
-    const cardObject = {
-        imageUrl : "https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg",
-        name : "Devesh Sutahr",
-        country : "IN",
-        role :"Web Developer",
-        company : "Delhi",
-        noOfSessions : 5,
-        reviews : 4.4,
-        experience : 2,
-        avgAttendance : 80
-      }
 
 
     useEffect(()=>{
@@ -63,11 +52,11 @@ function Mentor() {
           <MentorSearch mentors={mentors} setFilteredMentors={setFilteredMentors}></MentorSearch>
           <Navbar selected={selected} setSelected= {setSelected}/>
         </div>
-        <div className="flex justify-center px-16 gap-4 flex-wrap">
+        <div className="flex justify-center px-16 gap-4 flex-wrap mx-[7vw]">
           {
             filteredMentors.map((mentor,index)=>{
               console.log(mentor);
-              return <MentorCard key={index} cardData={mentor}></MentorCard>
+              return <><MentorCard key={index} cardData={mentor}></MentorCard></>
             })
           }
         </div>
