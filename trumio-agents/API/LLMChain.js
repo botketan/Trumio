21640cc385e9130688bd0612466736d6d2ee9671
Shri <psshrivathsa784@gmail.com>
2023-12-09 19:33:47 +0530

    Minor UI fixes

--- trumio-agents/API/LLMChain.js
+++ trumio-agents/API/LLMChain.js
@@ -8,7 +8,7 @@ const openai = new openaiapi.OpenAI({
 
 // Contains the msgs array which has the past msg history with the bot.
 let msgs = [
-  { role:"system", content: "You are an assistant. Give me a consistant answer to the following text that is present below"}
+  { role:"system", content: "You are an assistant. Give me a consistant answer to the following text that is present below. Also make the message crisp and short"}
 ];
 
 // Keep it async , we get a promise object
