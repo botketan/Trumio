7adbe7cb659b0111fffecff61df31910d6a42fe2
Achintya <achintya7567@gmail.com>
2023-12-10 20:46:32 +0530

    cia

--- backend/routes/ciaRoutes.js
+++ backend/routes/ciaRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { createChat,postChat, postHelper} from "../controllers/ciaTest.js";
+import { createChat,postChat, postHelper} from "../controllers/cia.js";
 
 router.post("/createChat", createChat);
 router.post("/postChat", postChat);
